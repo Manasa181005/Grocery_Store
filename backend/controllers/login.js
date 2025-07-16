251077d682546a5176191d2924d8d988c823b67e
Manasa181005 <23091a05b7@rgmcet.edu.in>
2025-07-16 18:52:55 +0530

    Update login.js

--- backend/controllers/login.js
+++ backend/controllers/login.js
@@ -4,11 +4,11 @@ let login = asyncHandler(async (req, res) => {
     const { email, password } = req.body
     console.log(email, password)
     let user = await User.findOne({ email: email, password: password })
-    if (user) {
+    if (user) {  
         res.json({ status: true, user: user });
     }
     else {
         res.json({ status: false });
     }
 })
-module.exports = { login }
+module.exports =  { login }
