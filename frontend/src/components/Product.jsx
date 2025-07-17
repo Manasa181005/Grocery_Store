a45b75118f271103f681b3fac7f20a24a36241e9
Manasa181005 <23091a05b7@rgmcet.edu.in>
2025-07-17 19:14:11 +0530

    Product.jsx

--- frontend/src/components/Product.jsx
+++ frontend/src/components/Product.jsx
@@ -2,10 +2,10 @@ import React, { useEffect, useState } from 'react'
 import ProductsItem from './ProductsItem'
 import { useContext } from 'react'
 import { UserContext } from '../Store'
-export default function Product(props) {
+export default function Product(props)    {
   const SearchcartList=useContext(UserContext).SearchcartList
   const [product, setProduct]=useState([]);
-  useEffect(()=>{
+  useEffect(() =>{
     fetch(process.env.REACT_APP_API_KEY + "getProducts").then((res)=>{
       return res.json();
     }).then((data)=>{
@@ -13,18 +13,20 @@ export default function Product(props) {
         setProduct(data.products)
       }
     })
+    
   },[])
   return (
     <div className='Product' style={{ marginTop: props["Margin"].margin }} >
       {
-        props.type === "Shop" ? (SearchcartList.map((ele) => {
+        props.type === "Shop" ? (SearchcartList.map((ele) =>{
           return <ProductsItem data={ele} key={ele.name} fulldata={ele}></ProductsItem>
         })) : (product.map((ele) => {
           // console.log(ele.name)
           return <ProductsItem data={ele} key={ele.name} fulldata={ele}></ProductsItem>
-        }))
+        } ) )
 
       }
     </div>
+    
   )
 }
