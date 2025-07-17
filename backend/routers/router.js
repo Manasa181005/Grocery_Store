5eacabeb28871cc5688a2dd9cac4669a0c589d3a
Manasa181005 <23091a05b7@rgmcet.edu.in>
2025-07-17 18:29:03 +0530

    router.js

--- backend/routers/router.js
+++ backend/routers/router.js
@@ -1,10 +1,11 @@
-const express=require("express");
-const {signUp} = require("../controllers/signUp.js");
+const express=require("express") ;
+const {signUp} = require("../controllers/signUp.js") ;
 const {login} = require("../controllers/login.js");
 const {product} = require("../controllers/product.js");
 const {searchProduct} = require("../controllers/searchProducts.js");
 const {payment} = require("../controllers/payment.js");
-const router=express.Router();
+const router=express.Router()  ;
+
 router.post("/signUp",signUp)
 router.post("/login",login)
 router.get("/getProducts",product)
