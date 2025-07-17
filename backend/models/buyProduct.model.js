bde437edf8e5a94db4454a4823e515348ead28f6
Manasa181005 <23091a05b7@rgmcet.edu.in>
2025-07-17 18:24:16 +0530

    buyProduct.model.js

--- backend/models/buyProduct.model.js
+++ backend/models/buyProduct.model.js
@@ -4,23 +4,24 @@ const buyProduct = new mongoose.Schema({
     userid: {
         type: String,
         required: true
-    },
+        
+    } ,
     paymentid: {
         type: String,
         required: true
-    },
+    } ,
     name: {
         type: String,
         required: true
-    },
+    } ,
     category: {
         type: String,
         required: true
-    },
+    } ,
     price:{
         type: Number,
         required: true
     }
-})
+} )
 const BuyProduct = new mongoose.model("BuyProduct", buyProduct)
 module.exports = { BuyProduct }
